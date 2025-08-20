abe923f4b51fccfcfbd9a9af64d8422ce758c1fc
Rizqi Fajri <rizqifajri51@gmail.com>
2025-08-20 15:18:39 +0700

    feat: add configuration ignore type

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    serverComponentsExternalPackages: ['pdf-parse', 'mammoth'],
+    serverComponentsExternalPackages: ["pdf-parse", "mammoth"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
